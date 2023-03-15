5290406ebb1ab585ad086474848392f53681dd83
Dominguez <ajd219@miami.edu>
2023-03-15 08:41:32 -0400

    fetched products from database. refactored routes. added custom error handler middleware, and custom 404 error handler middleware.

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 import cors from "cors";
-
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config()
 
@@ -16,23 +16,19 @@ app.use(cors({
     origin:process.env.FRONTEND_URL
 }))
 
+app.use("/api/products", productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.get("/", (req,res)=>{
     res.send("API is running")
 });
 
-app.get("/api/products", (req,res)=>{
-  res.json(products);
-})
-
-app.get("/api/products/:id", (req,res)=>{
-    const product=products.find(p=>p._id ===req.params.id)
-    res.json(product);
-})
-
 const PORT=process.env.PORT;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
 
 
